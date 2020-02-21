18f1909b146ffaf7d60d69fe24c7ce4a7cd54945
Xiangyu Zhang <xz556@cornell.edu>
2020-02-21 11:12:22 -0500

    check the typo in Exercise-13. Nothing learned.

---
+++ Exercise-13/a.py
@@ -0,0 +1,10 @@
+import sys
+
+script, first_arg, second_arg, third_arg = sys.argv
+
+print(f"""
+The script name is {script},
+first argument is {first_arg},
+second argument is {second_arg},
+third argument is {third_arg}.
+""")
