7a31672ae9136f30aaee28188fe607d66d24eb07
Xiangyu Zhang <xz556@cornell.edu>
2020-02-22 10:49:23 -0500

    learn to call help() with my own module; *.pyc file generated when import my own module

---
+++ Exercise-25/ex25.py
@@ -0,0 +1,12 @@
+def add(a, b):
+	"""
+	This is an implementation of add function.
+	"""
+	return a + b
+
+def subtract(a, b):
+	"""
+	This is an implementation of subtract function.
+	"""
+	return a - b
+
