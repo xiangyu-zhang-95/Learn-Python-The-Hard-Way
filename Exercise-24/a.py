7497e2d0298775a58e8be5248daefda6495a8efb
Xiangyu Zhang <xz556@cornell.edu>
2020-02-22 10:16:14 -0500

    fix typo in Exercise-24

--- Exercise-24/a.py
+++ Exercise-24/a.py
@@ -14,4 +14,18 @@ print("--------------------")
 print(poem)
 print("--------------------")
 
+five = 10 - 2 + 3 - 6
+print(f"this should be five: {five}")
+
+def secret_formula(started):
+	jelly_beans = started * 500
+	jars = jelly_beans // 1000
+	crates = jars // 100
+	return jelly_beans, jars, crates
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+print(f"with a start of {start_point}")
+print(f"we have {beans} beans, {jars} jars, and {crates} crates.")
 
