239cd9f7ba2519d48ed10ea7bda852af6bbba4fc
Xiangyu Zhang <xz556@cornell.edu>
2020-02-22 09:59:30 -0500

    printing paragraph with """ will prepend&append one empty line

---
+++ Exercise-24/a.py
@@ -0,0 +1,17 @@
+print("Let's practice something.")
+print('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhether there is none.
+"""
+
+print("--------------------")
+print(poem)
+print("--------------------")
+
+
