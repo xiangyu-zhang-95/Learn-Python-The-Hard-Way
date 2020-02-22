29efdb817c1103af2d76c15f11cd0f176e56b0f8
Xiangyu Zhang <xz556@cornell.edu>
2020-02-22 11:22:32 -0500

    nothing learn. type in ex26.

---
+++ Exercise-28/ex28.py
@@ -0,0 +1,18 @@
+assert (True and True)
+assert not (False and True)
+assert (1 == 1 and 2 ==2)
+assert "test" == "test"
+assert 1 == 1 or 2 != 1
+assert True and 1 == 1
+assert not (False and 0 != 0)
+assert True or 1 == 1
+assert not "test" == "testing"
+assert not (1 != 0 and 2 == 1)
+assert "test" != "testing"
+assert not (True and False)
+assert 10 == 1 or 1000 == 1000
+assert 1 != 10 or 3 == 4
+assert not ("testing" == "testing" and "Zed" == "Cool Guy")
+assert 1 == 1 and not ("testing" == 1 or 1 == 0)
+assert not ("chunky" == "bacon" and not (3 ==4 or 3 == 3))
+assert not (3 == 3 and not ("testing" == "testing" or "Python" == "Fun"))
